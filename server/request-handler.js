b5ca1539ef8f2a625de26412dd4b5f41cb057e63
JadeVasun <jadevasun@gmail.com>
2017-09-06 20:50:15 -0700

    completed chatterbox-server

--- server/request-handler.js
+++ server/request-handler.js
@@ -11,39 +11,28 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
+var obj = {
+  results: [],
+};
+
+
 
 var requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
-
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
-
-  // The outgoing status.
   var statusCode = 200;
 
   // See the note below about CORS headers.
-  var headers = defaultCorsHeaders;
+  
 
   // Tell the client we are sending them plain text.
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
+  
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
+  
 
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
@@ -52,40 +41,46 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  if (request.url !== '/classes/messages') {
-    response.writeHead(404, headers);
-    response.end();
-  }
-  
-  if (request.method === 'GET' && request.url === '/classes/messages') {
-    var obj = {
-      results: []
-    };
-  
-    response.end(JSON.stringify(obj));
-  }
+  // if (request.url !== '/classes/messages') {
+   
+  //   response.writeHead(404, headers);
+  //   response.end();
+  // }
 
-  if (request.method === 'POST' && request.url === '/classes/messages') {
+  // if (request.method === 'GET') {
+    
+   
+    
+  // }
+  if (!request.url.includes('/classes/messages')) {
+    statusCode = 404;
+  }
+  if (request.method === 'POST') {
     //console.log(request, 'testing');
-    response.end()
+    request.on('data', (chunk) => {
+      obj.results.push(JSON.parse(chunk));
+    });
+
+    statusCode = 201;
+    // response.end(JSON.stringify(obj));
   }
+  var defaultCorsHeaders = {
+    'access-control-allow-origin': '*',
+    'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'access-control-allow-headers': 'content-type, accept',
+    'access-control-max-age': 10 // Seconds.
+  };
+  var headers = defaultCorsHeaders;
+
+  headers['Content-Type'] = 'application/json';
+
+  response.writeHead(statusCode, headers);
+  
+  response.end(JSON.stringify(obj));
 };
 
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
-var defaultCorsHeaders = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10 // Seconds.
-};
+
+
 
 exports.requestHandler = requestHandler;
-exports.defaultCorsHeaders = defaultCorsHeaders;
+//exports.defaultCorsHeaders = defaultCorsHeaders;
